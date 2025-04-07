d6f275f43caa9cebfa94aa2a465511b90d389f42
Jadesola Adeagbo <jadesolaadeagbo@gmail.com>
2025-04-07 01:50:31 +0100

    feat: Added redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/insights/uri', // Replace 'hashtag' with the default hashtag
+        permanent: true,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
